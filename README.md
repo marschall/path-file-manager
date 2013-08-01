a11b383ad79b487647b2c369868198197545627e
Philippe Marschall <philippe.marschall@gmail.com>
2013-08-01 11:47:21 +0200

    bump

---
+++ README.md
@@ -0,0 +1,10 @@
+PathJavaFileManager
+===================
+A JSR-199 JavaFileManager that uses java.nio.Path instead of java.io.File.
+
+Supported
+---------
+
+Not Supported
+-------------
+Options
---
+++ src/test/java/com/github/marschall/pathjavafilemanager/PathJavaFileManagerTest.java
@@ -0,0 +1,14 @@
+package com.github.marschall.pathjavafilemanager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PathJavaFileManagerTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
