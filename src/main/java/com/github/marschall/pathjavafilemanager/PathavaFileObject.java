cb8dfe80cfe38697cfa26b8788b85bb13653035c
Philippe Marschall <philippe.marschall@gmail.com>
2013-08-01 12:48:08 +0200

    - start with JavaFileObject

---
+++ src/main/java/com/github/marschall/pathjavafilemanager/PathavaFileObject.java
@@ -0,0 +1,106 @@
+package com.github.marschall.pathjavafilemanager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.NestingKind;
+import javax.tools.JavaFileObject;
+
+final class PathavaFileObject implements JavaFileObject {
+  
+  private final Path path;
+  private final Kind kind;
+  
+  PathavaFileObject(Path path, Kind kind) {
+    this.path = path;
+    this.kind = kind;
+  }
+
+  @Override
+  public URI toUri() {
+    return this.path.toUri();
+  }
+
+  @Override
+  public String getName() {
+    return this.path.toString();
+  }
+
+  @Override
+  public InputStream openInputStream() throws IOException {
+    return Files.newInputStream(this.path);
+  }
+
+  @Override
+  public OutputStream openOutputStream() throws IOException {
+    return Files.newOutputStream(this.path);
+  }
+
+  @Override
+  public Reader openReader(boolean ignoreEncodingErrors) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Writer openWriter() throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public long getLastModified() {
+    try {
+      return Files.getLastModifiedTime(path).toMillis();
+    } catch (IOException e) {
+      // per contract
+      return 0L;
+    }
+  }
+
+  @Override
+  public boolean delete() {
+    try {
+      Files.delete(path);
+      return true;
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+  @Override
+  public Kind getKind() {
+    return this.kind;
+  }
+
+  @Override
+  public boolean isNameCompatible(String simpleName, Kind kind) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public NestingKind getNestingKind() {
+    // TODO check for $ in name and $123
+    return null;
+  }
+
+  @Override
+  public Modifier getAccessLevel() {
+    return null;
+  }
+
+}
--- src/test/java/com/github/marschall/pathjavafilemanager/PathJavaFileManagerTest.java
+++ src/test/java/com/github/marschall/pathjavafilemanager/PathJavaFileManagerTest.java
@@ -1,12 +1,24 @@
 package com.github.marschall.pathjavafilemanager;
 
+import static javax.tools.JavaFileObject.Kind.SOURCE;
+import static javax.tools.StandardLocation.SOURCE_PATH;
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 
+import javax.tools.DiagnosticListener;
+import javax.tools.FileObject;
 import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
 import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
 import javax.tools.ToolProvider;
 
 import org.junit.Test;
@@ -24,13 +36,26 @@ public class PathJavaFileManagerTest {
       Files.createDirectory(source);
       Files.createDirectory(target);
       
+      // TODO copy files to source
+      
       JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
       try (JavaFileManager fileManager = new PathJavaFileManager(source, target)) {
         
+        FileObject sibling = null;
+        JavaFileObject helloWorldInvoker = fileManager.getJavaFileForOutput(SOURCE_PATH, "com.github.marschall.pathjavafilemanager.HelloWorldInvoker", SOURCE, sibling);
+        JavaFileObject helloWorld = fileManager.getJavaFileForOutput(SOURCE_PATH, "com.github.marschall.pathjavafilemanager.HelloWorld", SOURCE, sibling);
+        
+        Writer out = new StringWriter();
+        DiagnosticListener<? super JavaFileObject> diagnosticListener = null; // use the compiler's default method for reporting diagnostics
+        Iterable<String> options = null; // no options
+        Iterable<String> classes = null; // means no class names
+        List<JavaFileObject> compilationUnits = Arrays.asList(helloWorldInvoker, helloWorld);
+        
+        CompilationTask task = compiler.getTask(out, fileManager, diagnosticListener, options, classes, compilationUnits);
+        if (!task.call()) {
+          fail(out.toString());
+        }
       }
-      
-      
-      
     }
   }
 
